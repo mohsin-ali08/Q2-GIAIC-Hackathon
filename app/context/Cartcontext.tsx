8c01c719864b334f3c03ba001f4059d8165b4d14
mohsin ali <mohsinalisurhio08@gmail.com>
2024-12-30 23:31:39 +0500

    update changes

--- app/context/Cartcontext.tsx
+++ app/context/Cartcontext.tsx
@@ -4,11 +4,11 @@ import { createContext, useState, ReactNode } from "react";
 export const CartContext = createContext<{
   showCart: boolean;
   setShowCart: React.Dispatch<React.SetStateAction<boolean>>;
-  qty: number;
+ 
 }>({
   showCart: false,
   setShowCart: () => {},
-  qty: 1,
+  
 });
 
 interface CartProviderProps {
@@ -17,10 +17,9 @@ interface CartProviderProps {
 
 export const CartProvider = ({ children }: CartProviderProps) => {
   const [showCart, setShowCart] = useState(false);
-  const [qty, setQty] = useState(1);
 
   return (
-    <CartContext.Provider value={{ showCart, setShowCart, qty }}>
+    <CartContext.Provider value={{ showCart, setShowCart }}>
       <div>{children}</div>
     </CartContext.Provider>
   );
